241ee4c93fbf07ab60ddc34d52a306fdeb58e36d
Valery Chumakou <vchumakou@gmail.com>
2024-06-18 08:39:07 -0400

    getAdditional card and computerMove methods were added

--- src/main/java/org/bura/bura_card_game/Bura.java
+++ src/main/java/org/bura/bura_card_game/Bura.java
@@ -9,9 +9,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -26,11 +25,12 @@ public class Bura extends Application {
     private HBox gamePanel;
     private ImageView imageView;
     private Image image;
-    private  Image backImage;
+    private Image backImage;
 
-    public Bura () {
+    public Bura() {
 
     }
+
     @Override
     public void start(Stage stage) throws Exception {
         mainPane = new BorderPane();
@@ -38,14 +38,24 @@ public class Bura extends Application {
         mainPane.setStyle("-fx-background-color:red;");
         backImage = new Image("file:C:/Users/Valery Chumakou/OneDrive/Documents/Java Projects/Bura_Card_Game/images/back_image/b3.gif");
 
+        gamePanel = new HBox(10);
+        gamePanel.setPadding(new Insets(10));
+        gamePanel.setAlignment(Pos.CENTER);
+
         computerPanel = new HBox(10);
         computerPanel.setPadding(new Insets(10));
-        dealComputerCards();
 
+        sidePanel = new VBox(10);
+        sidePanel.setPadding(new Insets(100));
+        mainPane.setRight(sidePanel);
 
         playerPanel = new HBox(10);
         playerPanel.setPadding(new Insets(20));
+
+        getMoreCards();
         dealPlayerCards();
+        dealComputerCards();
+
 
         tablePanel = new HBox(10);
         tablePanel.setPadding(new Insets(10));
@@ -61,10 +71,11 @@ public class Bura extends Application {
         HBox bottomPanel = new HBox();
         bottomPanel.setSpacing(10);
         bottomPanel.setAlignment(Pos.CENTER);
-        bottomPanel.getChildren().addAll(tablePanel,playerPanel);
+        bottomPanel.getChildren().addAll(tablePanel, playerPanel);
         mainPane.setBottom(bottomPanel);
         mainPane.setRight(sidePanel);
         mainPane.setCenter(gamePanel);
+
 
         Scene scene = new Scene(mainPane, 750, 500);
         stage.setScene(scene);
@@ -73,20 +84,72 @@ public class Bura extends Application {
     }
 
 
-    public void dealComputerCards() {
+    public void getMoreCards() {
         File folder = new File("images");
-        File [] files = folder.listFiles();
-        Card [] cards = new Card[3];
+        File[] files = folder.listFiles();
+        Random random = new Random();
+        Image backimage = new Image("file:C:/Users/Valery Chumakou/OneDrive/Documents/Java Projects/Bura_Card_Game/images/back_image/b3.gif");
+        ImageView backimageView = new ImageView(backimage);
+        Image triumphImage = new Image(files[random.nextInt(files.length)].toURI().toString());
+        ImageView triumphImageView = new ImageView(triumphImage);
+        triumphImageView.setFitHeight(100);
+        triumphImageView.setFitWidth(100);
 
-        for (int i = 0; i<3; i++) {
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(backimageView, triumphImageView);
+        mainPane.setCenter(vBox);
+        imageView = triumphImageView;
+        Image image = new Image(files[random.nextInt(files.length)].toURI().toString());
+        sidePanel.getChildren().add(new ImageView(image));
+        sidePanel.getChildren().add(new ImageView(backimage));
+    }
+
+
+    public void dealComputerCards() {
+
+        File folder = new File("images");
+        File[] files = folder.listFiles();
+        Card[] cards = new Card[3];
+
+
+        for (int i = 0; i < 3; i++) {
             cards[i] = new Card(new ImageView(backImage));
+            ((ImageView) cards[i].getImageView()).setUserData(cards[i]);
             cards[i].getImageView().setVisible(true);
             cards[i].getImageView().setFitHeight(100);
             cards[i].getImageView().setFitWidth(100);
-            computerPanel.getChildren().add(cards[i].getImageView());
+            ((ImageView) cards[i].getImageView()).setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(javafx.scene.input.MouseEvent event) {
+                    Node node = (Node) event.getSource();
+                    Card selectedCard = (Card) node.getUserData();
+                    if (selectedCard != null && playerPanel.getChildren().contains(node)) {
+                        gamePanel.getChildren().add(selectedCard.getImageView());
+                        playerPanel.getChildren().remove(node);
+                        computerMode();
+                    }
+                }
+            });
+        }
+        playerPanel.getChildren().clear();
+        for (int i = 0; i < 3; i++) {
+             playerPanel.getChildren().add(cards[i].getImageView());
         }
     }
 
+    public void computerMode() {
+        File folder = new File("images");
+        File [] files = folder.listFiles();
+        Random random = new Random();
+        File randomFile = files[random.nextInt(files.length)];
+        Image image = new Image(randomFile.toURI().toString());
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(100);
+        imageView.setFitHeight(100);
+        Card card = new Card(imageView);
+        card.setImageView(imageView);
+        gamePanel.getChildren().add(card.getImageView());
+    }
     public void dealPlayerCards() {
         File folder  = new File("images");
         File [] files = folder.listFiles();
@@ -108,7 +171,8 @@ public class Bura extends Application {
                     Node node = (Node) event.getSource();
                     Card selectedCard = (Card) node.getUserData();
                     if (selectedCard != null && playerPanel.getChildren().contains(node)) {
-                        System.out.println("Card selected");
+                        gamePanel.getChildren().add(selectedCard.getImageView());
+                        playerPanel.getChildren().remove(node);
 
                     }
                 }
--- src/main/java/org/bura/bura_card_game/RenderedCard.java
+++ src/main/java/org/bura/bura_card_game/RenderedCard.java
@@ -3,8 +3,10 @@ package org.bura.bura_card_game;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.ImageObserver;
+
 
 public class RenderedCard extends Card {
     protected boolean isHighSuit;
@@ -13,6 +15,7 @@ public class RenderedCard extends Card {
 
     public RenderedCard(int cv, char s, boolean v) {
         super(cv, s, v);
+
 
         dim = new Dimension();
         dim.setSize(73, 97);
@@ -27,8 +30,11 @@ public class RenderedCard extends Card {
     }
 
 
-
     }
+
+
+
+
 
 
 
